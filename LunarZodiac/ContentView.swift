29d205ce011b8b4399b8d11288cab5258118b895
tomasliivak <tomasliivak@gmail.com>
2026-02-02 17:44:15 -0500

    Initial Commit

---
+++ LunarZodiac/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LunarZodiac
+//
+//  Created by Tomas Liivak on 2/2/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LunarZodiac/LunarZodiacApp.swift
@@ -0,0 +1,17 @@
+//
+//  LunarZodiacApp.swift
+//  LunarZodiac
+//
+//  Created by Tomas Liivak on 2/2/26.
+//
+
+import SwiftUI
+
+@main
+struct LunarZodiacApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
