16cbaa608422a718540e28bd6c31852c66c17156
tomasliivak <tomasliivak@gmail.com>
2026-02-02 18:11:56 -0500

    Completed Project

--- LunarZodiac/ContentView.swift
+++ LunarZodiac/ContentView.swift
@@ -8,12 +8,50 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var animalNames = ["RAT", "OX", "TIGER", "RABBIT", "DRAGON", "SNAKE", "HORSE", "GOAT", "MONKEY", "ROOSTER", "DOG", "PIG"]
+    @State private var currentYear = 2026
+    @State private var imageNumber = 6
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text(String(currentYear))
+                .font(.largeTitle)
+                .fontWeight(.black)
+                .foregroundStyle(.red)
+            Spacer()
+            Image(animalNames[imageNumber])
+                .resizable()
+                .scaledToFit()
+            Text("\(animalNames[imageNumber])")
+                .font(.largeTitle)
+                .fontWeight(.black)
+            HStack{
+                Button {
+                    imageNumber -= 1
+                    currentYear -= 1
+                    if imageNumber < 0 {
+                        imageNumber = 11
+                    }
+                } label: {
+                    Image(systemName: "chevron.left")
+                }
+                Spacer()
+                Button {
+                    imageNumber += 1
+                    currentYear += 1
+                    if imageNumber > 11 {
+                        imageNumber = 0
+                    }
+                } label: {
+                    Image(systemName: "chevron.right")
+                }
+            }
+            .font(.largeTitle)
+            .fontWeight(.black)
+            .buttonStyle(.glassProminent)
+            .tint(.red)
+            .foregroundStyle(.white)
+            
         }
         .padding()
     }
